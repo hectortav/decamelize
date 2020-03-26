7d25db8ad582798d575f9a8f66f577935dd81f89
hectortav <tavhector@gmail.com>
2020-03-26 21:58:49 +0200

    Remove unneeded prints

--- test/decamelize_test.dart
+++ test/decamelize_test.dart
@@ -1,17 +1,11 @@
 import '../lib/decamelize.dart';
 
 bool main() {
-	print(decamelize('easyTest','_'));
 	assert(decamelize('easyTest','_') == 'easy_test');
-	print(decamelize('EasyTest','_'));
 	assert(decamelize('EasyTest','_') == 'easy_test');
-	print(decamelize('justATest','_'));
 	assert(decamelize('justATest','_') == 'just_a_test');
-	print(decamelize('JustATest','_'));
 	assert(decamelize('JustATest','_') == 'just_a_test');
-	print(decamelize('JUST A TEST','_'));
 	assert(decamelize('JUST A TEST','_') == 'just a test');
-	print(decamelize('JUSTATEST','_'));
 	assert(decamelize('JUSTATEST','_') == 'justatest');
 	return true;
 }
