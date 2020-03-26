2f01e6e3e781b7a24ba678796c5e7037c136e3da
hectortav <tavhector@gmail.com>
2020-03-26 21:50:30 +0200

    Add check for delimiter and capitalized string

--- lib/decamelize.dart
+++ lib/decamelize.dart
@@ -1,12 +1,18 @@
 import 'dart:core';
 
 String decamelize(String input, String del) {
-	return input.splitMapJoin(
-		RegExp('((([a-z])|([0-9]))(?=[A-Z]))'), 
-		onMatch: (match) => '${match.group(0)}$del',
-		onNonMatch: (noMatch) => '${noMatch.substring(0)}').splitMapJoin(
-			RegExp('((([a-z])|([0-9]))((A|I)([A-Z]))(([a-z])|([0-9])))'),
-			onMatch: (match) => '${match.group(0)}$del',
-			onNonMatch: (noMatch) => '${noMatch.substring(0)}'
-		).toLowerCase();
+	if (input == input.toUpperCase())
+		return input.toLowerCase();
+	if (RegExp('([a-z]|[A-Z]|[0-9])').hasMatch(del))
+		throw('$del is not acceptable');
+
+	String output = input.splitMapJoin(
+		RegExp('((([a-z])|([0-9]))((A|I)([A-Z]))(([a-z])|([0-9])))'), 
+		onMatch: (match) => '${match.group(0)[0]}$del${match.group(0)[1]}$del${match.group(0)[2]}${match.group(0)[3]}',
+		onNonMatch: (noMatch) => '${noMatch.substring(0)}');
+	return output.splitMapJoin(
+		RegExp('(([a-z]|[0-9])[A-Z]([a-z]|[0-9]))'),
+		onMatch: (match) => '${match.group(0)[0]}$del${match.group(0)[1]}${match.group(0)[2]}',
+		onNonMatch: (noMatch) => '${noMatch.substring(0)}')
+		.toLowerCase();
 }
